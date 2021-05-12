7a15ffa52cc247fb0abbd6e0834130b66d38de1c
XHung09 <74760526+XHung09@users.noreply.github.com>
2021-05-12 20:56:59 +0700

    update toBinaryExpression to work with 3 and 4 variables (#8)
    * modify methods in Minterm class
    
    * Complete toBinaryExpression() in Minterm class

--- QuineMcCluskeyProject/src/hust/soict/ict/quinemccluskey/model/minterm/Minterm.java
+++ QuineMcCluskeyProject/src/hust/soict/ict/quinemccluskey/model/minterm/Minterm.java
@@ -1,19 +1,27 @@
 package hust.soict.ict.quinemccluskey.model.minterm;
 
+import hust.soict.ict.quinemccluskey.model.Variable;
+
 public class Minterm extends Implicant {
 
 	// Getters and setters
-	public Minterm(String minterm) {
-		super(minterm);
+	public Minterm(String implicant) {
+		super(implicant);
 	}
 
-	// Methods
-	public void toBinaryExpression() {
-		String binEx[] = super.implicant.split("\\s");			// split by white space
-		
-		for (int i = 0; i < binEx.length; i++) {
-			super.binaryExpression = Integer.toBinaryString(Integer.parseInt(binEx[i])) + " ";
-		}	
+	public Minterm(String implicant, String binaryExpression) {
+		super(implicant, binaryExpression);
 	}
 	
+	// Methods
+	public void toBinaryExpression() {
+		binaryExpression = Integer.toBinaryString(Integer.parseInt(implicant));
+		
+		while (binaryExpression.length() < Variable.numberDigits) {
+			binaryExpression = "0" + binaryExpression;
+		}
+	}	
+
 }
+	
+
