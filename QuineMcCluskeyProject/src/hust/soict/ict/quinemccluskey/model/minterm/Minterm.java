e99972ffa304491faee56da57c44375cc5e41b7a
Hung <hung.tx194772@sis.hust.edu.vn>
2021-05-12 20:47:43 +0700

    Complete toBinaryExpression() in Minterm class

--- QuineMcCluskeyProject/src/hust/soict/ict/quinemccluskey/model/minterm/Minterm.java
+++ QuineMcCluskeyProject/src/hust/soict/ict/quinemccluskey/model/minterm/Minterm.java
@@ -5,36 +5,23 @@ import hust.soict.ict.quinemccluskey.model.Variable;
 public class Minterm extends Implicant {
 
 	// Getters and setters
-	public Minterm(String minterm) {
-		super(minterm);
+	public Minterm(String implicant) {
+		super(implicant);
 	}
 
-	// Methods
-	public void toBinaryExpression() {
-		String binEx[] = implicant.split("\\s");			// split by white space
-		
-		for (int i = 0; i < binEx.length; i++) {
-			String temp = Integer.toBinaryString(Integer.parseInt(binEx[i]));
-			
-			switch (Variable.numberDigits) {
-				case 4:
-					if (temp.length() < 4) {
-						while (temp.length() < 4) {
-							temp = "0" + temp;
-						}
-					}
-					
-				case 3:
-					if (temp.length() < 3) {
-						while (temp.length() < 3) {
-							temp = "0" + temp;
-						}
-					}
-					
-				default:
-					binaryExpression = temp + " ";
-			}
-		}	
+	public Minterm(String implicant, String binaryExpression) {
+		super(implicant, binaryExpression);
 	}
 	
+	// Methods
+	public void toBinaryExpression() {
+		binaryExpression = Integer.toBinaryString(Integer.parseInt(implicant));
+		
+		while (binaryExpression.length() < Variable.numberDigits) {
+			binaryExpression = "0" + binaryExpression;
+		}
+	}	
+
 }
+	
+
