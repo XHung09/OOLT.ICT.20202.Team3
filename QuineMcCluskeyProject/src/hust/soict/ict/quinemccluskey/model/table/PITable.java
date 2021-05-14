3f6aa4cf271b4a6d812aedfa0c4df704f0ddcc9a
hieutran29 <hieu.xteen28@gmail.com>
2021-05-14 18:05:00 +0700

    added implementation fro IntermediateTable

--- QuineMcCluskeyProject/src/hust/soict/ict/quinemccluskey/model/table/PITable.java
+++ QuineMcCluskeyProject/src/hust/soict/ict/quinemccluskey/model/table/PITable.java
@@ -2,4 +2,16 @@ package hust.soict.ict.quinemccluskey.model.table;
 
 public class PITable implements Table {
 
+    @Override
+    public void generate() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void display() {
+        // TODO Auto-generated method stub
+        
+    }
+
 }
