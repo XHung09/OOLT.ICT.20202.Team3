b168c12e284e2233d9b20d294f4c87a66131c564
Nguyen Tran Minh Tuan <mtn.191020001@gmail.com>
2021-05-18 15:40:31 +0700

    OutputFunctionTest not Checked

--- QuineMcCluskeyProject/src/hust/soict/ict/quinemccluskey/test/OutputFunctionTest.java
+++ QuineMcCluskeyProject/src/hust/soict/ict/quinemccluskey/test/OutputFunctionTest.java
@@ -1,5 +1,22 @@
 package hust.soict.ict.quinemccluskey.test;
 
-public class OutputFunctionTest {
-    
+import java.util.ArrayList;
+
+import hust.soict.ict.quinemccluskey.model.minterm.CombinedImplicant;
+import hust.soict.ict.quinemccluskey.model.minterm.Implicant;
+import hust.soict.ict.quinemccluskey.model.output.OutputFunction;
+public class OutputFunctionTest extends OutputFunction{
+	public static void main(String[] args) {	// test case
+		OutputFunctionTest out = new OutputFunctionTest();
+		ArrayList<Implicant> buff = new ArrayList<Implicant>();
+		Implicant a = new CombinedImplicant("1", "0001");
+		Implicant b = new CombinedImplicant("3", "0101");
+	
+		buff.add(a);
+		buff.add(b);
+		
+		String str = out.toCharacterEquation(buff);
+		System.out.println("Before deMorgan: " + str);
+		System.out.println("After deMorgan: " + out.DeMorgan(str));		
+	}
 }
