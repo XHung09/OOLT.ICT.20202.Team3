1c936ab58eb31b3e610100fb786a5e607c271452
hieutran29 <hieu.xteen28@gmail.com>
2021-05-29 21:17:57 +0700

    change Table from interface to abstract

--- QuineMcCluskeyProject/src/hust/soict/ict/quinemccluskey/model/table/Table.java
+++ QuineMcCluskeyProject/src/hust/soict/ict/quinemccluskey/model/table/Table.java
@@ -1,11 +1,14 @@
 package hust.soict.ict.quinemccluskey.model.table;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import hust.soict.ict.quinemccluskey.model.minterm.Implicant;
 
-public interface Table {
-    void generate();
-    void display();
-    List<Implicant> getPrimeImplicants();
+public abstract class Table {
+	protected List<Implicant> primeImplicants = new ArrayList<> ();
+	
+	public abstract void generate();
+	public abstract void display();
+	public abstract List<Implicant> getPrimeImplicants();
 }
